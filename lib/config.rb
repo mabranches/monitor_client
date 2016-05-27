f099e1850b7061ba40b9e570af9698005f00e79c
Manoel Rebelo Abranches <abranches@gmail.com>
2016-05-27 06:45:06 -0300

    class to hold configuration

---
+++ lib/config.rb
@@ -0,0 +1,11 @@
+require 'yaml'
+class Config
+  attr_reader :api_base, :instance_id, :seconds
+  def initialize
+    yml = YAML.load(File.read('config/application.yml'))
+    @api_base = yml['api_base']
+    @seconds = yml['seconds']
+    @instance_id = 'dasda'#RestClient.get('http://169.254.169.254/latest/meta-data/instance-id')
+  end
+
+end
