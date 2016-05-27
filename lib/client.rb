b401892d1958f8236a0f2d577aa020d6266ccdc5
Manoel Rebelo Abranches <abranches@gmail.com>
2016-05-27 06:45:26 -0300

    client to api

---
+++ lib/client.rb
@@ -0,0 +1,12 @@
+require 'rest-client'
+
+class Client
+   def initialize(config)
+     @api_base = config.api_base
+     @instance_id = config.instance_id
+   end
+   
+   def send_usage(usage)
+      RestClient.post("#{@api_base}/instances/#{@instance_id}", usage)
+   end
+end
