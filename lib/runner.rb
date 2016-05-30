0085e77f234e1abcd0e03309b712cadde1daa0d1
Manoel Rebelo Abranches <abranches@gmail.com>
2016-05-30 20:41:12 -0300

    Move logic to a runner class for tests

---
+++ lib/runner.rb
@@ -0,0 +1,40 @@
+require 'logger'
+class Runner
+
+  def initialize(config, collectors)
+    @config = config
+    @log = Logger.new(config.log_path)
+    @collectors = collectors.collect{|collector_class|  collector_class.new(@log)}
+  end
+
+  def run
+    while true do
+      RunnerBlock.run_block(@config, @collectors, @log)
+    end
+  end
+end
+
+class RunnerBlock
+
+  def self.run_block(config, collectors, log)
+    client = Client.new(config)
+    usage = {instance_id: config.instance_id}
+    sleep(config.seconds)
+      collectors.each do |collector|
+        begin
+          collector.collect
+          usage[collector.type] = collector.usage
+        rescue => e
+          puts "Could not collect with #{collector.class.name}. #{e.message}"
+          log.error("Could not collect with #{collector.class.name}. #{e.message}")
+        end
+      end
+      begin
+        client.send_usage(usage)
+      rescue => e
+        puts "Could not send usage status to api. #{e.message}"
+        log.error("Could not send usage status to api. #{e.message}")
+      end
+  end
+
+end
--- monitor_client.rb
+++ monitor_client.rb
@@ -1,30 +1,16 @@
 require 'rest-client'
 require './lib/config.rb'
 require './lib/collector.rb'
-Dir["./lib/collectors/*.rb"].each {|file| require file }
 require './lib/client.rb'
+require './lib/runner.rb'
+Dir["./lib/collectors/*.rb"].each {|file| require file }
 
 config = Config.new
-client = Client.new(config)
-collectors = []
-collectors << DiskCollector.new
-collectors << MemCollector.new
-collectors << CPUCollector.new
-collectors << ProcessCollector.new
+collectors = [
+  DiskCollector,
+  MemCollector,
+  CPUCollector,
+  ProcessCollector
+]
 
-usage = {instance_id: config.instance_id}
-
-while true do
-
-  sleep(config.seconds)
-  collectors.each do |collector|
-    collector.collect
-    usage[collector.type] = collector.usage || 0
-  end
- 
-  if (!client.send_usage(usage))
-    log.error("Could not send usage status to api. #{collector.error}")
-  end
-    
-end
-
+Runner.new(config, collectors).run
