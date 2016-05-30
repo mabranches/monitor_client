eff33f768ed93e8021fef3b2f41e61d41d146cd4
Manoel Rebelo Abranches <abranches@gmail.com>
2016-05-30 20:30:01 -0300

    Remove .bundle after rpm creation

--- build_rpm.sh
+++ build_rpm.sh
@@ -4,3 +4,4 @@ bundle clean
 fpm --after-install rpm_files/conf_initd.sh --after-remove rpm_files/rm_initd.sh \
   -f -s dir -t rpm -n monitor_client -v 1.0.0 -a noarch \
   --prefix /opt/monitor_client monitor config rpm_files  Gemfile Gemfile.lock lib/ monitor_client.rb vendor .bundle
+rm -r .bundle
