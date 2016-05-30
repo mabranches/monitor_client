20af46432c4a0fc1b9055861ad955d77d84b81c6
Manoel Rebelo Abranches <abranches@gmail.com>
2016-05-30 20:47:29 -0300

    add log path to config class

--- lib/config.rb
+++ lib/config.rb
@@ -1,10 +1,11 @@
 require 'yaml'
 class Config
-  attr_reader :api_base, :instance_id, :seconds
+  attr_reader :api_base, :instance_id, :seconds, :log_path
   def initialize
     yml = YAML.load(File.read('config/application.yml'))
     @api_base = yml['api_base']
     @seconds = yml['seconds']
+    @log_path = yml['log_path']
     @instance_id = RestClient.get('http://169.254.169.254/latest/meta-data/instance-id')
   end
 
