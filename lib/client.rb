15096a5a70fd224f98dce628b390779169895237
Manoel Rebelo Abranches <abranches@gmail.com>
2016-05-31 09:21:22 -0300

    send following rails standard

--- lib/client.rb
+++ lib/client.rb
@@ -5,8 +5,9 @@ class Client
      @api_base = config.api_base
      @instance_id = config.instance_id
    end
-   
+
    def send_usage(usage)
-      RestClient.post("#{@api_base}/instances/#{@instance_id}", usage)
+      RestClient.post("#{@api_base}/instances/#{@instance_id}",
+        {instance: usage})
    end
 end
